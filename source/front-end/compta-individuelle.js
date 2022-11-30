25510c29588441b0680d3ba923fb5da72c5ee145
David Bruant <davidbruant@protonmail.com>
2022-11-30 18:39:37 +0100

    Parsing des données du Crédit Mutuel

--- source/front-end/compta-individuelle.js
+++ source/front-end/compta-individuelle.js
@@ -1,32 +1,19 @@
-import {buffer, text} from "d3-fetch"
-import {dsvFormat} from "d3-dsv"
+//@ts-check
 
-const SEPARATEUR_CREDIT_MUTUEL = ";";
+import {buffer} from "d3-fetch"
 
-const parse = dsvFormat(SEPARATEUR_CREDIT_MUTUEL).parse
+import {parseCSVCreditMutuel} from "../import-banque/crédit-mutuel.js"
+
+
 
 const bqEBseptOct2022P = buffer("./données/relevés-bancaires/2022-09-10(jusqu'au 10-10).csv")
-    .then(arrayBuffer => {
-
-        /* encoding par défaut du Crédit Mutuel
-            Il n'y a évidemment aucune documentation sur laquelle on peut compter
-            ni de changelog, donc ptèt que ça changera,
-            sûrement sans prévenir
-            On verra à ce moment-là
-        */
-        const decoder = new TextDecoder("windows-1252"); 
-        const str = decoder.decode(arrayBuffer); 
-        console.log("str", str)
-
-        return parse(str)
-    })
+    .then(parseCSVCreditMutuel)
 
 bqEBseptOct2022P.then(data => {
     console.log("data", data)
 })
 
 document.addEventListener("DOMContentLoaded", e => {
-
     
 
 
